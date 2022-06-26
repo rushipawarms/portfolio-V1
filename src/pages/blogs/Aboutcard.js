779233010f47d8967143675e26db0792fa3995c1
rushikesh Pawar <rushikesh.pawarms@gmail.com>
2022-06-26 10:56:11 +0530

    adding new experience

--- src/pages/blogs/Aboutcard.js
+++ src/pages/blogs/Aboutcard.js
@@ -2,10 +2,12 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import { ImPointRight } from "react-icons/im";
 import logo from '../../assets/tcs-twotter.jpg'
+import logo1 from '../../assets/nagarro.gif'
+import logo2 from '../../assets/nagarro1.webp'
 import './about.css'
 function AboutCard() {
   return (
-   
+   <div style={{ display: 'flex',flexWrap:'wrap'}}>
     <div className="container" >
       <div className="card" >
         <div className="image"  >
@@ -13,7 +15,7 @@ function AboutCard() {
         </div>
         <div className="content" >
           <h1 style={{fontWeight:"bold", display:"flex",alignItems: 'start'}}>System Engineer</h1>
-          <h5 style={{display:"flex",alignItems: 'start', fontStyle:'italic'}}> Sept 2019-Present</h5>
+          <h5 style={{display:"flex",alignItems: 'start', fontStyle:'italic'}}> Sept 2019-June 2022</h5>
         <ul >
             <li >
                 Working as UI devloper.
@@ -28,6 +30,30 @@ function AboutCard() {
         </div>
       </div>    
     </div>
+    <div className="container" >
+      <div className="card" >
+        <div className="image"  >
+          <img href = "#" src ={logo2} style={{height:"100%", width:"100%"}}/>
+        </div>
+        <div className="content" >
+          <h1 style={{fontWeight:"bold", display:"flex",alignItems: 'start'}}>Engineer</h1>
+          <h5 style={{display:"flex",alignItems: 'start', fontStyle:'italic'}}> June 2022-Present</h5>
+        <ul >
+            <li >
+                Working as UI devloper.
+            </li>
+            <li>
+            Collaborates with frontend team.
+            </li>
+            <li>
+            Building things on React,redux.
+            </li>
+        </ul>
+        </div>
+      </div>    
+    </div>
+   </div>
+   
  
   );
 }
